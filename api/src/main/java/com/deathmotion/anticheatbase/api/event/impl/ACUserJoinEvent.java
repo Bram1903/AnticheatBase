c1cb7b884ee2edd4f93a039dc841746d89560369
Bram <bram@bramdekker.com>
2025-10-31 10:19:34 +0100

    Improved: JavaDocs for the ACUserJoinEvent & ACUserQuitEvent

--- api/src/main/java/com/deathmotion/anticheatbase/api/event/impl/ACUserJoinEvent.java
+++ api/src/main/java/com/deathmotion/anticheatbase/api/event/impl/ACUserJoinEvent.java
@@ -2,11 +2,18 @@ package com.deathmotion.anticheatbase.api.event.impl;
 
 import com.deathmotion.anticheatbase.api.models.ACUser;
 
-/*
-    Called when an anticheat user joins the server (doesn't include players with a bypass)
+/**
+ * Called when an anticheat user joins the server.
+ * <p>
+ * This event does not include players with a bypass.
  */
 public class ACUserJoinEvent extends ACUserEvent {
 
+    /**
+     * Constructs a new {@code ACUserJoinEvent}.
+     *
+     * @param user the {@link ACUser} who joined the server
+     */
     public ACUserJoinEvent(ACUser user) {
         super(user);
     }
--- api/src/main/java/com/deathmotion/anticheatbase/api/event/impl/ACUserQuitEvent.java
+++ api/src/main/java/com/deathmotion/anticheatbase/api/event/impl/ACUserQuitEvent.java
@@ -2,11 +2,18 @@ package com.deathmotion.anticheatbase.api.event.impl;
 
 import com.deathmotion.anticheatbase.api.models.ACUser;
 
-/*
-    Called when an anticheat user quit the server (doesn't include players with a bypass)
+/**
+ * Called when an anticheat user quits the server.
+ * <p>
+ * This event does not include players with a bypass.
  */
 public class ACUserQuitEvent extends ACUserEvent {
 
+    /**
+     * Constructs a new {@code ACUserQuitEvent}.
+     *
+     * @param user the {@link ACUser} who quit the server
+     */
     public ACUserQuitEvent(ACUser user) {
         super(user);
     }
