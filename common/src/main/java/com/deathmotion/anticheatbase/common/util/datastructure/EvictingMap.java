55e549bb28973c69c70bba35d4e15050cf181c5c
Bram <bram@bramdekker.com>
2025-10-31 23:58:27 +0100

    Added: Math related utility classes

---
+++ common/src/main/java/com/deathmotion/anticheatbase/common/util/datastructure/EvictingMap.java
@@ -0,0 +1,18 @@
+package com.deathmotion.anticheatbase.common.util.datastructure;
+
+import java.util.TreeMap;
+
+public class EvictingMap<K, V> extends TreeMap<K, V> {
+    private final int maxSize;
+
+    public EvictingMap(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        if (size() >= maxSize) remove(firstKey());
+        return super.put(key, value);
+    }
+
+}
